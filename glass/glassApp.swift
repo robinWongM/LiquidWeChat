7b4114fb431cc9ea2653f2a076299da4c4034142
Robin Wong <chaowang0313@gmail.com>
2025-06-10 20:19:28 +0800

    Initial Commit

---
+++ glass/glassApp.swift
@@ -0,0 +1,17 @@
+//
+//  glassApp.swift
+//  glass
+//
+//  Created by Robin Wong on 2025/6/10.
+//
+
+import SwiftUI
+
+@main
+struct glassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
